6fb21067d0d5b3b312c49dd4c1d78931025f240c
VlamirD <vladicha0912@gmail.com>
2016-08-09 15:12:21 -0500

    -capturando datos -implemtentando la funcion detalles

--- www/conecta.php
+++ www/conecta.php
@@ -2,41 +2,55 @@
 //configurando servidor
 $serve=mysql_connect('localhost','root','');
 if(!$serve){echo 'error';}
-$db=mysql_select_db('temperaturaapp',$serve);
+$atri=20;
+$db=mysql_select_db('temperaturaAPP',$serve);
 $dias = array('Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado');
 $mes=array('01'=>'Enero','02'=>'Febrero','03'=>'Marzo','04'=>'Abril','05'=>'Mayo','06'=>'Junio','07'=>'Julio','08'=>'Agosto','09'=>'Septiembre','10'=>'Octubre','11'=>'Nobiembre','12'=>'Diciembre');
 //incluir datos
-//
-//
-//listar datos
-/*if($_GET['valor']=='actual'){
-    $sql="select * from dato_climatico where fecha=curdate()";
+if (isset($_GET['valor'])) {
+    echo $_GET['valor'];
+}
+if($_GET['valor']=='especifico'){
+    $resultado=$_GET['atributo'];
+    //echo $resultado;
+    $sql="SELECT * FROM dato_climatico where id= $resultado";
     $re=mysql_query($sql,$serve);
     $num=mysql_num_rows($re);
     if($num>0){
         while($lista=mysql_fetch_object($re)){
-            echo "<br>fecha:</b>{$lista->fecha}</br>";
-            echo "<b>temperatura:</b>{$lista->temperatura} <b><br><b>presion:</b>{$lista->presion}</br><br>";
-        }
-    }
-//}*/
-if(isset($_GET['valor'])){
-    if($_GET['valor']=='listar') $sql="SELECT * FROM dato_climatico ORDER BY id DESC LIMIT 1";
-    if($_GET['valor']=='listar5') $sql="SELECT * FROM dato_climatico ORDER BY id DESC LIMIT 5";
-    if($_GET['valor']=='detalles')$sql="SELECT * FROM dato_climatico ORDER BY id DESC LIMIT 1";
-    $re=mysql_query($sql,$serve);
-    $num=mysql_num_rows($re);
-    if($num>0){
-        while($lista=mysql_fetch_object($re)){
-            echo "<div class='ui-btn ui-btn-corner-all bg-cian'>";
+            echo "<div id='especifico".$lista->id."' class='ui-btn ui-btn-corner-all bg-cian'>";
             $i = strtotime($lista->fecha);
             echo "<p>".$dias[jddayofweek(cal_to_jd(CAL_GREGORIAN, date("m",$i),date("d",$i), date("Y",$i)) , 0 )].",".$mes[date("m",$i)]." ".date("d",$i)."</p>";
             echo "<p></b>{$lista->temperatura}</p>";
             $temp=$lista->temperatura;
             if($temp<=15){
-                //echo "<p></b>{$lista->presion}</p>";
                 echo "<img src='img/nublado.png'>";
                 echo "<p>Nublado</p>";
+            }
+            echo "</div>";
+        } 
+    }
+    else{
+        echo 'no hay temperaturas';
+    }
+}
+//listar datos
+if($_GET['valor']=='listar5'||$_GET['valor']=='listar'){
+    if ($_GET['valor']=='listar5')$sql="SELECT * FROM dato_climatico ORDER BY id DESC LIMIT 5";
+    if ($_GET['valor']=='listar')$sql="SELECT * FROM dato_climatico ORDER BY id DESC LIMIT 1";
+    $re=mysql_query($sql,$serve);
+    $num=mysql_num_rows($re);
+    if($num>0){
+        while($lista=mysql_fetch_object($re)){
+            echo "<div id='detalle".$lista->id."' class='ui-btn ui-btn-corner-all bg-cian' onclick='detalles($lista->id)'>";
+            $i = strtotime($lista->fecha);
+            echo "<p>".$dias[jddayofweek(cal_to_jd(CAL_GREGORIAN, date("m",$i),date("d",$i), date("Y",$i)) , 0 )].",".$mes[date("m",$i)]." ".date("d",$i)."</p>";
+            echo "<p></b>{$lista->temperatura}</p>";
+            $temp=$lista->temperatura;
+            if($temp<=15){
+                echo "<img src='img/nublado.png'>";
+                echo "<p>Nublado</p>";
+                echo "<input id='opcion".$lista->id."' type='hidden' value='".$lista->id."'>";
                 echo "<a href='#pagina3'></a>";
             }
             echo "</div>";
@@ -46,4 +60,5 @@ if(isset($_GET['valor'])){
         echo 'no hay temperaturas';
     }
 }
+
 ?>
--- www/js/funcion.js
+++ www/js/funcion.js
@@ -1,44 +1,12 @@
+server='';
 $(document).ready(function(){
-    var $server='http://localhost:80/xampp/Proyecto/www/';
-    $('#pagina1').on('load',function(){
-            $.ajax({
-            type:"get",
-            dataType:'html',
-            url:$server+"/conecta.php",
-            data:"valor=actual",
-            success:function(data){
-                $('#actual').html();
-                intel.xdk.notification.alert('valor mostrado','aviso1','ok');
-            }
-        });
-    });
-    function listartemp(){
-        $.ajax({
-            type:"get",
-            dataType:'html',
-            url:$server+"/conecta.php",
-            data:"valor=listar"+5,
-            success:function(data){
-                $('#listar1').html(data);
-            }
-        });
-    }
-    function detalles(dato){
-        $.ajax({
-            type:"get",
-            dataType:'html',
-            url:$server+"/conecta.php",
-            data:"valor=detalles",
-            success:function(data){
-                $('#detalles').html(data);
-            }
-        });
-    }
+    //var $server='http://localhost/xampp/Proyecto/www/';
+    server='http://localhost/xampp/Proyecto/www/';
     function lista(dato){
         $.ajax({
             type:"get",
             dataType:'html',
-            url:$server+"/conecta.php",
+            url:server+"/conecta.php",
             data:"valor=listar"+dato,
             success:function(data){
                 $('#listar').html(data);
@@ -49,14 +17,28 @@ $(document).ready(function(){
         $.ajax({
             type:"get",
             dataType:'html',
-            url:$server+"/conecta.php",
+            url:server+"/conecta.php",
             data:"valor=listar"+5,
             success:function(data){
                 $('#listar1').html(data);
             }
         });
     }
-    detalles();
+    //detalles();
     listartemp();
     lista('');
 });
+function detalles(valor){
+        var id=$('#opcion'+valor).val();
+        //console.log(id);
+        //alert(""+id);
+        $.ajax({
+            type:"get",
+            dataType:'html',
+            url:server+"/conecta.php",
+            data:"atributo="+id+"&valor=especifico",
+            success:function(data){
+                $('#detalles').html(data);
+            }
+        });
+    }
